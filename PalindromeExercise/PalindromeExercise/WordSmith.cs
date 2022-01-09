1b2a317b5af8b76563437dcd3d3e98b833a92163
tray190 <belltracy411@gmail.com>
2022-01-08 20:36:21 -0600

    update

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith(string word)
+        {
+
+        }   
+        public bool IsAPalindrome(string word)
+        {
+            var reversed = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word.ToLower()[i];
+            }
+
+            if (reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+        }
+
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("RaceCar", true)]
+        [InlineData("Hello", false)]
+        public void Palindrome(string word, bool expected)
         {
-
+            // Arrange
+            var wordSmith = new WordSmith(word);
+            //act
+            var actual = wordSmith.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
